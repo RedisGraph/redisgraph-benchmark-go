362f043195d11252dc8d7e60ca3503708789e62c
filipecosta90 <filipecosta.90@gmail.com>
2021-07-12 11:47:30 +0100

    [add] Enabled GRAPH.RO_QUERY command support.

--- multi-query.go
+++ multi-query.go
@@ -16,11 +16,11 @@ func sample(cdf []float32) int {
 	return bucket
 }
 
-func prepareCommandsDistribution(cmds []string, cmdRates []float64) (int, []float32) {
+func prepareCommandsDistribution(queries arrayStringParameters, cmds []string, cmdRates []float64) (int, []float32) {
 	var totalDifferentCommands = len(cmds)
 	var totalRateSum = 0.0
 	var err error
-	for i, rawCmdString := range benchmarkQueries {
+	for i, rawCmdString := range queries {
 		cmds[i] = rawCmdString
 		if i >= len(benchmarkQueryRates) {
 			cmdRates[i] = 1
@@ -38,11 +38,11 @@ func prepareCommandsDistribution(cmds []string, cmdRates []float64) (int, []floa
 		log.Fatalf("Total ratio should be 1.0 ( currently is %f )", totalRateSum)
 	}
 	// probability density function
-	if len(benchmarkQueryRates) > 0 && (len(benchmarkQueryRates) != len(benchmarkQueries)) {
-		log.Fatalf("When specifiying -query-rate parameter, you need to have the same number of -query and -query-rate parameters. Number of time -query ( %d ) != Number of times -query-params ( %d )", len(benchmarkQueries), len(benchmarkQueryRates))
+	if len(benchmarkQueryRates) > 0 && (len(benchmarkQueryRates) != (len(benchmarkQueries) + len(benchmarkQueriesRO))) {
+		log.Fatalf("When specifiying -query-rate parameter, you need to have the same number of -query/-query-ro and -query-rate parameters. Number of time -query ( %d ) != Number of times -query-params ( %d )", len(benchmarkQueries), (len(benchmarkQueryRates) + len(benchmarkQueriesRO)))
 	}
-	pdf := make([]float32, len(benchmarkQueries))
-	cdf := make([]float32, len(benchmarkQueries))
+	pdf := make([]float32, len(queries))
+	cdf := make([]float32, len(queries))
 	for i := 0; i < len(cmdRates); i++ {
 		pdf[i] = float32(cmdRates[i])
 		cdf[i] = 0
